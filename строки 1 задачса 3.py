cb1633cfb95233ec764c498ba0f75be2ac08ff4e
FillkaTep1 <154609961+FillkaTep1@users.noreply.github.com>
2023-12-25 16:16:09 +0500

    Add files via upload

---
+++ строки 1 задачса 3.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+
+def top3(st):
+    counter_top3 = Counter(st.replace(' ', '')).most_common(3)
+    return ', '.join([f'{letter} - {cnt}' for letter, cnt in counter_top3])
+print(top3('Закон Ома'))
