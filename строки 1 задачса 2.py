1b17964bd4a4cea3c347d1efaa1541a8f474f884
FillkaTep1 <154609961+FillkaTep1@users.noreply.github.com>
2023-12-25 16:14:05 +0500

    Add files via upload

---
+++ строки 1 задачса 2.py
@@ -0,0 +1,7 @@
+def first_last(letter, st):
+    first = st.find(letter)
+    if first < 0:
+        return None, None
+    last = st.rfind(letter)
+    return first, last
+print(first_last('b', 'bbum'))
